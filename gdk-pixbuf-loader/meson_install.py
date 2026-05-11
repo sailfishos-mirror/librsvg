7904287a301498868c65d084358d269abd5a0359
Ross Burton <ross.burton@arm.com>
2026-05-11 12:34:53 +0100

    gdk-pixbuf-loader/meson_install: improve argument parsing and logic
    Make the arguments more explicit by changing the path arguments to be
    specified with options, such as --queryloaders <path>. Previously one of
    the paths had an option, but the others were order-based.
    
    Check that either all of the paths have been set or --show-cross-message
    has been passed. If this is not the case, print the usage and exit.
    
    Clean up Path usage, as the options are using pathlib.Path types in the
    parser another Path instance does not need to be created.
    
    None of this changes the underlying logic, just makes it more consistent
    and explicit.
    
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/1194>

--- gdk-pixbuf-loader/meson_install.py
+++ gdk-pixbuf-loader/meson_install.py
@@ -6,29 +6,28 @@ import os
 import subprocess
 import sys
 
-argparse = ArgumentParser('Deploy loaders.cache')
-
-argparse.add_argument('--gdk-pixbuf-moduledir', type=Path)
-argparse.add_argument('gdk_pixbuf_queryloaders', type=Path)
-argparse.add_argument('gdk_pixbuf_cache_file', type=Path)
-argparse.add_argument('--show-cross-message', action='store_true')
-
 if __name__ == '__main__':
+    argparse = ArgumentParser(description='Deploy loaders.cache. Pass required paths, or --show-cross-message.')
+    argparse.add_argument('--queryloaders', type=Path, metavar="PATH", help="gdk-pixbuf-queryloaders to run")
+    argparse.add_argument('--moduledir', type=Path, metavar="PATH", help="installed gdk-pixbuf module directory")
+    argparse.add_argument('--cache-file', type=Path, metavar="PATH", help="module cache file to write")
+    argparse.add_argument('--show-cross-message', action='store_true', help="tell the user to run query-loaders manually")
     args = argparse.parse_args()
 
-    cache_file: Path = args.gdk_pixbuf_cache_file
+    if not(args.show_cross_message or (args.queryloaders and args.moduledir and args.cache_file)):
+        argparse.print_help()
+        sys.exit(1)
 
     if args.show_cross_message or os.environ.get("DESTDIR"):
         print('*** Note: Please run gdk-pixbuf-queryloaders manually ' +
               'against the newly-built gdkpixbuf-svg loader', file=sys.stderr)
     else:
         env = os.environ.copy()
-        env['GDK_PIXBUF_MODULEDIR'] = Path(args.gdk_pixbuf_moduledir).as_posix()
-        with cache_file.open('w', encoding='utf-8') as f:
+        env['GDK_PIXBUF_MODULEDIR'] = args.moduledir.as_posix()
+        with args.cache_file.open('w', encoding='utf-8') as f:
             subprocess.run(
-                [Path(args.gdk_pixbuf_queryloaders).as_posix()],
+                [args.queryloaders.as_posix()],
                 env=env,
                 stdout=f,
                 check=True
             )
-
